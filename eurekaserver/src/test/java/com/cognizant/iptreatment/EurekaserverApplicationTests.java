998897c417e32d8e7bb0dfa03021244675f8217c
Siva OS <syskantechnosoft@users.noreply.github.com>
2021-09-30 13:58:27 +0530

    Add files via upload

---
+++ eurekaserver/src/test/java/com/cognizant/iptreatment/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.iptreatment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
